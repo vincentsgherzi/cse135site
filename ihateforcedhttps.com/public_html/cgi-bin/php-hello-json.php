e000b93059c4583329800ce1a180cbfd43cfdec7
vincentsgherzi <vsgherzi@ucsd.edu>
2023-04-29 23:43:13 -0700

    php json test

--- ihateforcedhttps.com/public_html/cgi-bin/php-hello-json.php
+++ ihateforcedhttps.com/public_html/cgi-bin/php-hello-json.php
@@ -1,17 +1,26 @@
-<?php
 
-    header('Content-Type: application/json');
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello, PHP</title>
+</head>
 
+<body>
+   
+    
+    <?php
     $message = "Hello World from PHP!";
 
     $date = date("Y-m-d");
     $ip_address = $_SERVER['REMOTE_ADDR'];
-    
+
     $response = array(
         "message" => $message,
         "date" => "Today's date is " . $date,
         "ipAddress" => $ip_address
     );
-    
-    echo json_encode($response);
-?>
+
+    echo $response;
+    ?>
+</body>
+</html>
