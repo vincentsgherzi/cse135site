0fb5abbb888cf8d961aa7ea8094b7f93958bf3c8
vincentsgherzi <vsgherzi@ucsd.edu>
2023-04-30 09:20:15 -0700

    update php redirect for post back

--- ihateforcedhttps.com/public_html/cgi-bin/php-state-demo.php
+++ ihateforcedhttps.com/public_html/cgi-bin/php-state-demo.php
@@ -3,7 +3,7 @@ session_start();
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['name'])) {
     $_SESSION['name'] = $_POST['name'];
-    header('Location: index.php'); 
+    header('Location: php-state-demo-two.php'); 
     exit;
 }
 ?>
