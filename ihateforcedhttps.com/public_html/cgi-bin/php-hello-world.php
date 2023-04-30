35f265488ef77cbadf32b7e5103e8579e7352be6
vincentsgherzi <vsgherzi@ucsd.edu>
2023-04-29 23:01:48 -0700

    adding php

---
+++ ihateforcedhttps.com/public_html/cgi-bin/php-hello-world.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello, PHP</title>
+</head>
+<body>
+	<?php
+		echo "<p>Hello World</p>";
+		echo "<p>This page was generated with the PHP programming language</p>";
+		echo "<p>This program was run at: " . date("Y-m-d") . "</p>";
+		echo "<p>Your current IP address is: " . $_SERVER['REMOTE_ADDR'] . "</p>";
+	?>
+</body>
+</html>
