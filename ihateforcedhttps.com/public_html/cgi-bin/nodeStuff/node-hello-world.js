7ba7ca87e8853b5ec6d33a1022fcd33e3feb80d9
vincentsgherzi <vsgherzi@ucsd.edu>
2023-04-30 13:47:09 -0700

    adding node things for hello w plz work

---
+++ ihateforcedhttps.com/public_html/cgi-bin/nodeStuff/node-hello-world.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const ip = require('ip');
+const app = express();
+
+app.get('/', (req, res) => {
+
+  const currentDate = new Date().toISOString().split('T')[0];
+  const currentIP = ip.address()
+
+  let respond = "<h1>Hello Node World</h1>"
+  respond+="<h2>Hello World</h2>"
+  respond+="<p>This page was generated with the Node.js and Express</p>"
+  respond+="<p>This program was run at: "+{currentDate}+"</p>"
+  respond+="<p>Your current IP address is: "+{currentIP}+"</p>"
+
+  res.send(respond)
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening at http://localhost:3000');
+});
