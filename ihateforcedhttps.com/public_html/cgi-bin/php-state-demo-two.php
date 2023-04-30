12e553b8aa74c4267d5b30bfd150ee15f7b843fe
vincentsgherzi <vsgherzi@ucsd.edu>
2023-04-30 09:09:49 -0700

    php state demo

---
+++ ihateforcedhttps.com/public_html/cgi-bin/php-state-demo-two.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Session Test</title>
+</head>
+<body>
+    <h1 align="center">Session Test page 2</h1>
+  
+    <form>
+        <label for="name-input">What is your name?</label>
+        <input type="text" id="name-input" name="name">
+        <button type="submit">Submit</button>
+    </form>
+
+    <li><a href="index.html">Return to Home</a></li>
+    <button>delete session</button>
+    
+</body>
+</html>
---
+++ ihateforcedhttps.com/public_html/cgi-bin/php-state-demo.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['name'])) {
+    $_SESSION['name'] = $_POST['name'];
+    header('Location: index.php'); 
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Session Test</title>
+</head>
+<body>
+    <h1 align="center">Session Test</h1>
+  
+    <form action="/cgi-bin/php-state-demo.php" method="POST">
+        <label for="name-input">What is your name?</label>
+        <input type="text" id="name-input" name="name">
+        <button type="submit">Submit</button>
+    </form>
+
+    <li><a href="/cgi-bin/php-state-demo-two.php">Link to page 2</a></li>
+
+</body>
+</html>
